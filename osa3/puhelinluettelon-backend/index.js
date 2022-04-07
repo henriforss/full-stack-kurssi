78826e57515bddf197e35877031d03ecebf77262
Henri Forss <hforss@gmail.com>
2022-04-07 10:56:55 +0300

    puhelinluettelon-backend, ekat askeleet

---
+++ osa3/puhelinluettelon-backend/index.js
@@ -0,0 +1,29 @@
+/* Hardcoded entries for testing. */
+let entries = [
+    {   id: 1,
+        name: "George Pullman",
+        number: 555-9876,
+    },
+    {   id: 2,
+        name: "King Kong",
+        number: 765-8877,
+    },
+    {   id: 3,
+        name: "Miss Terious",
+        number: 645-8765,
+    },
+    {   id: 4,
+        name: "Amanda Duckface",
+        number: 888-7766,
+    },
+]
+
+/* Create an express-object called "app" that is a function. */
+const express = require("express")
+const app = express()
+
+
+
+
+
+
