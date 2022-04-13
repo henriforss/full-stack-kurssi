cdcac3bb87cb26bad2f539e4222a99d125a10cad
Henri Forss <hforss@gmail.com>
2022-04-13 12:11:46 +0300

    3.15-3.18 valmis

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -8,14 +8,26 @@ const cors = require("cors")
 
 /* Import custom mongoose module from models/person.js. */
 const Person = require("./models/person")
+const { response } = require("express")
 
 /* Create an express-object called "app" that is a function. */
 const app = express()
 
+/* Create middleware errorHandler to handle all errors.
+Uses function next(). Enable last in script. */
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+    next(error)
+}
+
 /* Enable built-in middleware "json-parser" for parsing POST-requests. */
 app.use(express.json())
 
-/* Enable and configure external middleware "morgan" for console logging. */
+/* Enable and configure external middleware "morgan" for console logging.
+Very useful! */
 morgan.token("body", (request, response) => {
     const body = JSON.stringify(request.body)
     return body
@@ -28,7 +40,7 @@ app.use(cors())
 /* Enable built-in middleware static.  */
 app.use(express.static("build"))
 
-/* Get all entries in route "persons". */
+/* Get all entries in route "/api/persons". */
 app.get("/api/persons", (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -37,32 +49,39 @@ app.get("/api/persons", (request, response) => {
 
 /* Get info at "/info". */
 app.get("/info", (request, response) => {
-    const personsTotal = persons.length
-    const date = new Date()
-    response.send(`Phonebook has info on ${personsTotal} persons. <br/> 
-        ${date}`)
+    Person.countDocuments({}, (err, count) => {
+        console.log(`Number of persons: ${count}`)
+        const date = new Date()
+
+        response.send(`Phonebook has info on ${count} persons. <br>
+            ${date}`)
+    }) 
 })
 
 /* Get person by "id". If "id" is wrong, respond with status-code 404. */
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).send("Error 404: Page not found.")
-    }
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 /* Delete person by "id". */
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 /* Add new person. Requires json-parser to be enabled further up. */
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -82,10 +101,31 @@ app.post("/api/persons", (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
+
+/* Update person number. */
+app.put("/api/persons/:id", (request, response, next) =>{
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+/* Enable middleware errorHandler. Must always be last of middlewares. */
+app.use(errorHandler)
 
 /* Listen to port for connections/requests. */
 const PORT = process.env.PORT
--- osa3/puhelinluettelon-backend/models/person.js
+++ osa3/puhelinluettelon-backend/models/person.js
@@ -1,3 +1,6 @@
+/* This file creates a Person-model that is used for 
+all communication between app and database. */
+
 /* Import mongoose. */
 const mongoose = require("mongoose")
 
@@ -11,7 +14,7 @@ mongoose.connect(url)
         console.log("Connected to DB.")
     })
     .catch(error => {
-        console.log("Error connecting to DB", errror.message)
+        console.log("Error connecting to DB", error.message)
     })
 
 /* Create a schema. */
