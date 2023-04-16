56df72bd68b004f8b8fa18875dbd78f2b9a1b79d
Henri Forss <hforss@gmail.com>
2023-04-16 17:23:52 +0300

    9.10-11 done

--- osa9/patientor/patientor-backend/src/index.ts
+++ osa9/patientor/patientor-backend/src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import diagnosesRouter from "./routes/diagnoses";
+import patientsRouter from "./routes/patients";
 
 /* Create app. */
 const app = express();
 
 /* Middleware. */
 app.use(express.json());
+
+/* Fix cors. */
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  next();
+});
 
 /* Constants. */
 const PORT = 3001;
@@ -15,6 +23,10 @@ app.get("/api/ping", (_req, res) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.send("Hello World!");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
+
+app.use("/api/patients", patientsRouter);
 
 /* Listen. */
 app.listen(PORT, () => {
---
+++ osa9/patientor/patientor-backend/src/services/patientsService.ts
@@ -0,0 +1,18 @@
+import patientsData from "../../data/patients";
+import { Patient, NonSensitivePatient } from "../types";
+
+const getEntries = (): Patient[] => {
+  return patientsData;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patientsData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getEntries, getNonSensitiveEntries };
